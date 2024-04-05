0141425c9fa97633eba0e18b6ffc2f3dfbe1eb40
raylan <raylan@rd-dev.attlocal.net>
2024-04-04 22:28:23 -0400

    Added vanilla multi-static page serving

--- server.js
+++ server.js
@@ -1,14 +1,70 @@
-const express = require('express');
-const path = require('path');
-
-const app = express();
+import * as http from 'node:http';
+import * as fs from 'node:fs';
+import * as path from 'node:path';
 
 const PORT = 3000;
 
-app.use(express.static('to-do-vanilla'));
-app.use('/calculator', express.static('calculator-vanilla'));
-app.use('/to-do', express.static('to-do-vanilla'));
+const MIME_TYPES = {
+  default: 'application/octet-stream',
+  html: 'text/html; charset=UTF-8',
+  js: 'application/javascript',
+  css: 'text/css',
+  png: 'image/png',
+  jpg: 'image/jpg',
+  svg: 'image/svg+xml'
+};
 
-app.listen(PORT, () => {
-    console.log(`Listening on ${PORT}...`);
+const state = {
+  path: 'undefined'
+}
+
+const prepareFile = async (url) => {
+  const toBool = [() => true, () => false];
+
+  let STATIC_PATH;
+
+  if (url === '/' || url === '/to-do') {
+    url = '/';
+    state.path = 'to-do';
+  } else if (url === '/calculator') {
+    url = '/';
+    state.path = 'calculator';
+  };
+
+  if (state.path === 'to-do') {
+    STATIC_PATH = path.join(process.cwd(), './to-do-vanilla');
+  } else if (state.path === 'calculator') {
+    STATIC_PATH = path.join(process.cwd(), './calculator-vanilla');
+  };
+
+  const paths = [STATIC_PATH, url];
+
+  console.log(`Paths: ${paths}`);
+  console.log(`Current URL ${url}`);
+
+  if (url.endsWith('/')) {
+    paths.push('index.html');
+  } 
+
+  const filePath = path.join(...paths);
+  const pathTraversal = !filePath.startsWith(STATIC_PATH);
+
+  const exists = await fs.promises.access(filePath).then(...toBool);
+  const found = !pathTraversal && exists;
+  const streamPath = found ? filePath : STATIC_PATH + '/404.html';
+  const ext = path.extname(streamPath).substring(1).toLowerCase();
+  const stream = fs.createReadStream(streamPath);
+  return { found, ext, stream };
+
+};
+
+http.createServer(async (req, res) => {
+  console.log(req.url);
+  const file = await prepareFile(req.url);
+  const statusCode = file.found ? 200 : 400;
+  const mimeType = MIME_TYPES[file.ext] || MIME_TYPES.default;
+  res.writeHead(statusCode, { "Content-Type": mimeType });
+  file.stream.pipe(res);
+}).listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
 });
